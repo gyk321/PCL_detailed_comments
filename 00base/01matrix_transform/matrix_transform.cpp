1817bc8fa27af35446e9e757246e48d4c3ea8a37
lalala123 <1054060225@qq.com>
2023-02-09 01:30:29 +0800

    matrix_transform_detailed_comment

--- 00base/01matrix_transform/matrix_transform.cpp
+++ 00base/01matrix_transform/matrix_transform.cpp
@@ -19,7 +19,8 @@ void showHelp(char *program_name)
 int main(int argc, char **argv)
 {
 
-    // Show help 展示帮助信息
+    // Show help 展示帮助信息 
+    // pcl::console::find_switch 它可以在命令行中搜索是否存在特定的参数，如果存在则返回特定的参数值，如果不存在则返回默认值。
     if (pcl::console::find_switch(argc, argv, "-h") || pcl::console::find_switch(argc, argv, "--help"))
     {
         showHelp(argv[0]);
@@ -30,26 +31,30 @@ int main(int argc, char **argv)
     std::vector<int> filenames;
     bool file_is_pcd = false;
 
+    // 用于解析可能输入的文件扩展名参数
     filenames = pcl::console::parse_file_extension_argument(argc, argv, ".ply");
-
+    // 输入文件扩展名不是.ply
     if (filenames.size() != 1)
     {
+        // 继续读取文件扩展名
         filenames = pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
-
+        // 输入文件扩展名不是.pcd
         if (filenames.size() != 1)
         {
+            // 显示帮助信息
             showHelp(argv[0]);
             return -1;
         }
         else
         {
+            // 输入文件扩展名是.pcd
             file_is_pcd = true;
         }
     }
 
     // Load file | Works with PCD and PLY files 在参数中寻找pcd或者ply文件
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-
+    // 不能加载PCD文件，它会打印一条错误消息，然后显示帮助信息
     if (file_is_pcd)
     {
         if (pcl::io::loadPCDFile(argv[filenames[0]], *source_cloud) < 0)
@@ -62,6 +67,7 @@ int main(int argc, char **argv)
     }
     else
     {
+        // 不能加载PLY文件，它会打印一条错误消息，然后显示帮助信息
         if (pcl::io::loadPLYFile(argv[filenames[0]], *source_cloud) < 0)
         {
             std::cout << "Error loading point cloud " << argv[filenames[0]] << std::endl
@@ -103,12 +109,14 @@ int main(int argc, char **argv)
     /*  METHOD #2: Using a Affine3f ====================================第二种方法
     This method is easier and less error prone
   */
+    // 一个单位矩阵 主对角线上的元素值都是1
     Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
 
     // Define a translation of 2.5 meters on the x axis.
     transform_2.translation() << 2.5, 0.0, 0.0;
 
     // The same rotation matrix as before; theta radians around Z axis
+    // 以Vector3f的z轴(UnitZ)为中心，将角度设置为theta弧度，进行旋转
     transform_2.rotate(Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitZ()));
 
     // Print the transformation
@@ -118,6 +126,7 @@ int main(int argc, char **argv)
     // Executing the transformation
     pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZ>());
     // You can either apply transform_1 or transform_2; they are the same
+    // 将 source_cloud 点云应用 transform_2 的变换，得到一个新的 transformed_cloud 点云
     pcl::transformPointCloud(*source_cloud, *transformed_cloud, transform_2);
 
     // Visualization  可视化
@@ -128,19 +137,25 @@ int main(int argc, char **argv)
     // Define R,G,B colors for the point cloud
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler(source_cloud, 255, 255, 255);
     // We add the point cloud to the viewer and pass the color handler
+    // 使用viewer对象来添加一个名为原始云的点云，该点云的颜色由source_cloud_color_handler定义。
     viewer.addPointCloud(source_cloud, source_cloud_color_handler, "original_cloud");
 
+    // 创建一个用自定义颜色着色器（红色）设置的颜色处理器（类似于上色一个点云）
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> transformed_cloud_color_handler(transformed_cloud, 230, 20, 20); // Red
+    // 将一个点云（transformed_cloud）添加到viewer对象中，并指定它的颜色处理（transformed_cloud_color_handler）以及它的名称（"transformed_cloud"）
     viewer.addPointCloud(transformed_cloud, transformed_cloud_color_handler, "transformed_cloud");
-
+    // 添加一个坐标系（1.0）到查看器中以帮助视觉化点云（使用带有 “cloud” 标签的点云）
     viewer.addCoordinateSystem(1.0, "cloud", 0);
+    // 设置可视化查看器的背景颜色为深灰色（0.05, 0.05, 0.05, 0）。
     viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+    // 两个点云（ Original_cloud 和 transformed_cloud） 设置大小为 2
     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "original_cloud");
     viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "transformed_cloud");
     //viewer.setPosition(800, 400); // Setting visualiser window position
 
     while (!viewer.wasStopped())
-    { // Display the visualiser until 'q' key is pressed
+    { 
+        // Display the visualiser until 'q' key is pressed
         viewer.spinOnce();
     }
 
